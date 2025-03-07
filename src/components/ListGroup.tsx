242815a7fd4971ac4b36e31e36e42e087927866c
lfmutton <lfmutton@gmail.com>
2025-03-07 13:59:01 -0300

    starting the repository

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+function ListGroup() {
+  let items = ["China", "London", "Brazil", "Japan"];
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
