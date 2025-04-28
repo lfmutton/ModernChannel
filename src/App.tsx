e13e9c8761abc3b8f534b0f9ee1889d11635bff8
kauan <126937785+susHayato@users.noreply.github.com>
2025-04-27 22:59:41 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import WelcomeScreen from "./screens/WelcomePage";
 import LoginPage from "./screens/LoginPage";
 import SignUpPage from "./screens/SignUpPage";
 import HomePage from "./screens/HomePage";
+import ProfilePage from "./screens/ProfilePage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/ProfilePage" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
